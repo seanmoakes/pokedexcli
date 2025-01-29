fae8f02b4c6928689f18179a31f2fd1a8e4a1f74
Sean Moakes <mail@seanmoakes.com>
2025-01-29 19:07:29 +0000

    inspect command added

--- repl.go
+++ repl.go
@@ -76,6 +76,11 @@ func getCommands() map[string]cliCommand {
 			description: "Attempt to catch a Pokemon",
 			callback:    commandCatch,
 		},
+		"inspect": {
+			name:        "inspect <pokemon_name>",
+			description: "View details about a caught Pokemon",
+			callback:    commandInspect,
+		},
 		"map": {
 			name:        "map",
 			description: "Get the next page of locations",
